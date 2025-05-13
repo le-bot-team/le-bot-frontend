cb8aa98970c8bb3a17446540e1355fd7ae31e75e
ParticleG <particle_g@outlook.com>
2025-05-13 20:14:48 +0800

    - Fix compile error

--- src/boot/media-encoder.ts
+++ src/boot/media-encoder.ts
@@ -1,4 +1,7 @@
+import { defineBoot } from '#q-app/wrappers';
 import { register } from 'extendable-media-recorder';
 import { connect } from 'extendable-media-recorder-wav-encoder';
 
-await register(await connect());
+export default defineBoot(async () => {
+  await register(await connect());
+});
